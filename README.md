2fef671ee315b0063c5afe12a751623436a9728d
Kalpna Patel <techiedev@yahoo.com>
2013-03-04 12:26:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ror Tutorial: first application
+
+This is the first application for
+[*Ruby on Rails *](http://railstutorial.org/)
+by [Secret Squirrel](http://www.youtube.com/watch?v=-S_F9U9gNEQ).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a890811f5897d3956cf5bffcd6787812672714fe4f7e6562c0a2e95ea1a62994a7ec49de97985f40159d08091d57866acd98d25f5ba28622c463f414e503b42b'
