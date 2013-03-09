3768efbc8cef00a8a1a86b0a39ee1fb6b4e5d451
Kalpna Patel <techiedev@yahoo.com>
2013-03-08 21:46:48 -0500

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -32,5 +32,6 @@ group :test do
   gem 'rb-notifu', '0.0.4'
   gem 'win32console', '1.3.0'
   gem 'sqlite3', '1.3.5'
+  gem 'factory_girl_rails', '4.1.0'
 end
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+﻿FactoryGirl.define do
+  factory :user do
+    name     "Kalpna Patel"
+    email    "tech@my.net"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
